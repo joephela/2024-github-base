7b1892761da152ada31e3f670b6bc411a9e49057
Joseph Phelan <joephela@gmail.com>
2024-10-28 22:37:56 -0500

    update deploy command with vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    base: 'https://www.joephela.com',
+export default defineConfig(() => {
+  return {
     plugins: [react()],
   }
-
-  if (command === 'serve') {
-    config.base = '/'
-  }
-
-  return config
 })
